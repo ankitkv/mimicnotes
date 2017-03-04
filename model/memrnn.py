c77a203030eb3f15f44600b7c8cfb9205d4a2425
Ankit Vani <a@nevitus.org>
2017-03-04 15:32:09 -0500

    memrnn visualizations

--- model/memrnn.py
+++ model/memrnn.py
@@ -2,10 +2,17 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+from six.moves import xrange
+
 import tensorflow as tf
 
 import model
 import util
+
+try:
+    input = raw_input
+except NameError:
+    pass
 
 
 class MemoryRNNModel(model.TFModel):
@@ -31,8 +38,9 @@ class MemoryRNNModel(model.TFModel):
                                           (config.word_emb_size * config.num_blocks))
 
         # recurrence
-        _, last_state = tf.nn.dynamic_rnn(cell, embed, sequence_length=self.lengths,
-                                          swap_memory=True, dtype=tf.float32)
+        out, last_state = tf.nn.dynamic_rnn(cell, embed, sequence_length=self.lengths,
+                                            swap_memory=True, dtype=tf.float32)
+        self.step_probs = ((out[:, :, :label_space_size] * (1 - 2*1e-6)) + 1) / 2
         self.probs = ((last_state[:, :label_space_size] * (1 - 2*1e-6)) + 1) / 2
         loss = self.labels * -tf.log(self.probs) + (1. - self.labels) * -tf.log(1. - self.probs)
         self.loss = tf.reduce_mean(loss)
@@ -44,3 +52,42 @@ class MemoryRNNRunner(model.RecurrentNetworkRunner):
 
     def __init__(self, config, session):
         super(MemoryRNNRunner, self).__init__(config, session, ModelClass=MemoryRNNModel)
+
+    def visualize(self, verbose=True):
+        n_labels = 5
+        if self.config.query:
+            split = self.config.query
+        else:
+            split = 'test'
+        for batch in self.reader.get([split]):
+            ops = [self.model.probs, self.model.step_probs]
+            probs, step_probs = self.session.run(ops, feed_dict={self.model.notes: batch[0],
+                                                                 self.model.lengths: batch[1],
+                                                                 self.model.labels: batch[2]})
+            for i in xrange(probs.shape[0]):
+                print()
+                print('=== NEW NOTE ===')
+                doc_probs = step_probs[i]  # seq_len x labels
+                prob = [(j, p) for j, p in enumerate(probs[i]) if p > 0.5]
+                prob.sort(key=lambda x: -x[1])
+                prob = prob[:n_labels]
+                for label, _ in prob:
+                    label_prob = doc_probs[:, label]  # seq_len
+                    if batch[2][i, label]:
+                        verdict = 'correct'
+                    else:
+                        verdict = 'incorrect'
+                    print()
+                    print('LABEL (%s):' % verdict,
+                          self.vocab.aux_names['dgn'][self.vocab.aux_vocab['dgn'][label]])
+                    print('-----')
+                    for k, word in enumerate(batch[0][i, :batch[1][i]]):
+                        prob = label_prob[k]
+                        color = util.c.ENDC
+                        if prob > 0.66:
+                            color = util.c.OKGREEN
+                        if prob < 0.33:
+                            color = util.c.FAIL
+                        print(color + self.vocab.vocab[word] + util.c.ENDC, end=' ')
+                    print()
+                input('\n\nPress enter to continue ...\n')
