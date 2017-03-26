d75c9aeee1beef17aca21ecedc994166ea8a83b1
Ankit Vani <a@nevitus.org>
2017-03-26 18:36:13 -0400

    update torch runner as per the new API

--- util/torch_runner.py
+++ util/torch_runner.py
@@ -4,6 +4,7 @@ from __future__ import print_function
 
 import time
 
+import numpy as np
 import tensorflow as tf
 import torch
 from torch.autograd import Variable
@@ -79,24 +80,58 @@ class TorchRunner(util.Runner):
             loss.backward()
             self.optimizer.step()
             self.global_step += 1
-        probs = probs.data.cpu().numpy()
-        loss = loss.data.cpu().numpy()
+        self.probs = probs.data.cpu().numpy()
+        self.labels = labels
+        self.loss = loss.data.cpu().numpy()
+        end = time.time()
+        self.wps = n_words / (end - start)
+        self.accumulate()
+
+    def initialize_losses(self):
+        self.all_losses = []
+        self.all_probs = []
+        self.all_labels = []
+
+    def accumulate(self):
+        self.all_losses.append(self.loss)
+        self.all_probs.append(self.probs)
+        self.all_labels.append(self.labels)
+
+    def losses(self):
+        loss = np.mean(self.all_losses)
+        probs = np.concatenate(self.all_probs)
+        labels = np.concatenate(self.all_labels)
+        # micro-averaged stats
         p, r, f = util.f1_score(probs, labels, 0.5)
         ap = util.auc_pr(probs, labels)
-        auc = util.auc_roc(probs, labels)
+        try:
+            auc = util.auc_roc(probs, labels)
+        except ValueError:
+            auc = float('nan')
         p8 = util.precision_at_k(probs, labels, 8)
-        end = time.time()
-        wps = n_words / (end - start)
-        return loss, p, r, f, ap, auc, p8, wps
+        micro = [p, r, f, ap, auc, p8]
+        # macro-averaged stats
+        p, r, f = util.f1_score(probs, labels, 0.5, average='macro')
+        ap = util.auc_pr(probs, labels, average='macro')
+        try:
+            auc = util.auc_roc(probs, labels, average='macro')
+        except ValueError:
+            auc = float('nan')
+        p8 = util.precision_at_k(probs, labels, 8, average='macro')
+        macro = [p, r, f, ap, auc, p8]
+        return loss, micro, macro
 
     def sanity_check_loss(self, losses):
-        loss, p, r, f, ap, auc, p8, wps = losses
-        return f >= self.config.sanity_min and f <= self.config.sanity_max
+        loss, micro, macro = losses
+        p, r, f, ap, auc, p8 = micro
+        return ap >= self.config.sanity_min and ap <= self.config.sanity_max
 
-    def best_val_loss(self, loss):
+    def best_val_loss(self, losses):
         '''Compare loss with the best validation loss, and return True if a new best is found'''
-        if loss[4] >= self.best_ap:
-            self.best_ap = loss[4]
+        loss, micro, macro = losses
+        p, r, f, ap, auc, p8 = micro
+        if ap >= self.best_ap:
+            self.best_ap = ap
             return True
         else:
             return False
@@ -115,9 +150,26 @@ class TorchRunner(util.Runner):
                 print('Saved.')
 
     def loss_str(self, losses):
-        loss, p, r, f, ap, auc, p8, wps = losses
-        return "Loss: %.4f, Precision: %.4f, Recall: %.4f, F-score: %.4f, AUC(PR): %.4f, " \
-               "AUC(ROC): %.4f, Precision@8: %.4f, WPS: %.2f" % (loss, p, r, f, ap, auc, p8, wps)
+        loss, micro, macro = losses
+        loss_str = "Loss: %.4f" % loss
+        p, r, f, ap, auc, p8 = micro
+        micro_str = "Precision (micro): %.4f, Recall (micro): %.4f, F-score (micro): %.4f, " \
+                    "AUC(PR) (micro): %.4f, AUC(ROC) (micro): %.4f, Precision@8 (micro): %.4f" % \
+                    (p, r, f, ap, auc, p8)
+        p, r, f, ap, auc, p8 = macro
+        macro_str = "Precision (macro): %.4f, Recall (macro): %.4f, F-score (macro): %.4f, " \
+                    "AUC(PR) (macro): %.4f, AUC(ROC) (macro): %.4f, Precision@8 (macro): %.4f" % \
+                    (p, r, f, ap, auc, p8)
+        return ' | '.join([loss_str, micro_str, macro_str])
 
     def output(self, step, train=True):
-        print("GS:%d, S:%d.  %s" % (self.global_step, step, self.loss_str(self.losses())))
+        p, r, f = util.f1_score(self.probs, self.labels, 0.5)
+        ap = util.auc_pr(self.probs, self.labels)
+        try:
+            auc = util.auc_roc(self.probs, self.labels)
+        except ValueError:
+            auc = float('nan')
+        p8 = util.precision_at_k(self.probs, self.labels, 8)
+        print("GS:%d, S:%d.  Loss: %.4f, Precision: %.4f, Recall: %.4f, F-score: %.4f, "
+              "AUC(PR): %.4f, AUC(ROC): %.4f, Precision@8: %.4f, WPS: %.2f" %
+              (self.global_step, step, self.loss, p, r, f, ap, auc, p8, self.wps))
