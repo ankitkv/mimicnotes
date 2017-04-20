fff54dbaf0984204e03876d13b5ca474eb61191a
Ankit Vani <a@nevitus.org>
2017-04-20 19:49:05 -0400

    use probs to select indices

--- model/baseline2grnn.py
+++ model/baseline2grnn.py
@@ -16,6 +16,7 @@ class Baseline2GRNNRunner(util.TFRunner):
     '''Runner for the baseline to grounded RNN model.'''
 
     def __init__(self, config, session, verbose=True):
+        # TODO don't allow double initialization of vocab and reader
         super(Baseline2GRNNRunner, self).__init__(config, session)
         config.sliced_grnn = True  # this is a bit hacky..
         self.model = model.GroundedRNNModel(self.config, self.vocab, config.sliced_labels,
@@ -26,6 +27,7 @@ class Baseline2GRNNRunner(util.TFRunner):
         base_config.load_file = config.base_file
         base_config.sanity_epoch = -1
         base_config.save_every = -1
+        # FIXME fix loading of saved base file
         self.base_runner = model.BagOfWordsRunner(base_config, session)
         if config.emb_file:
             saver = tf.train.Saver([self.model.embeddings])
@@ -42,28 +44,13 @@ class Baseline2GRNNRunner(util.TFRunner):
             base_train = False
         self.base_runner.run_session(notes, lengths, labels, train=base_train)
         probs = self.base_runner.probs
-        print(probs.shape)
         ops = [self.model.loss, self.model.probs, self.model.global_step]
         if train:
             ops.append(self.model.train_op)
-        counts = labels.sum(0)  # TODO use probs instead
-        pos_indices = np.nonzero(counts)[0]
-        pos_labels = self.config.sliced_labels // 2
-        if pos_indices.shape[0] > pos_labels:
-            np.random.shuffle(pos_indices)
-            tmp_counts = counts[pos_indices]
-            indices = np.argpartition(-tmp_counts, pos_labels-1)[:pos_labels]
-            pos_indices = pos_indices[indices]
-        neg_labels = self.config.sliced_labels - pos_indices.shape[0]
-        neg_indices = np.arange(counts.shape[0], dtype=np.int)
-        neg_indices = np.setdiff1d(neg_indices, pos_indices, assume_unique=True)
-        if self.config.sample_uniform:
-            label_probs = None
-        else:
-            label_freqs = self.label_freqs[neg_indices]
-            label_probs = label_freqs / label_freqs.sum()
-        neg_indices = np.random.choice(neg_indices, [neg_labels], replace=False, p=label_probs)
-        indices = np.concatenate([pos_indices, neg_indices])
+        probs = -np.log(probs)
+        probs /= np.max(probs)
+        counts = probs.sum(0)
+        indices = np.argpartition(-counts, self.config.sliced_labels-1)[:self.config.sliced_labels]
         labels = labels[:, indices]
         feed_dict = {self.model.notes: notes, self.model.lengths: lengths,
                      self.model.slicing_indices: indices, self.model.labels: labels}
