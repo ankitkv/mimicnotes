6e8396a6a8894cec387f6c9bba18e5ea8a5b60b6
Ankit Vani <ankit.vani@featurex.ai>
2017-09-21 19:24:27 -0400

    fix bug

--- model/fconv.py
+++ model/fconv.py
@@ -77,6 +77,10 @@ class ConvEncoderModel(nn.Module):
         x = x.transpose(1, 2)
         x = x.sum(1)
 
+        lengths = Variable(torch.from_numpy(lengths).float().cuda(), volatile=tokens.volatile)
+        lengths = lengths.unsqueeze(1)
+        x = x / lengths
+
         x = self.fc2(x)
         return x
 
